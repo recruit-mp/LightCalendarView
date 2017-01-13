18e03e9490da1bde0162f21df5c650210987b0c2
recruit-mahayash <recruithd_140@r.recruit.co.jp>
2017-01-13 19:56:09 +0900

    [refactor] refactored DayLayout

--- library/src/main/kotlin/jp/co/recruit_mp/android/lightcalendarview/DayLayout.kt
+++ library/src/main/kotlin/jp/co/recruit_mp/android/lightcalendarview/DayLayout.kt
@@ -81,7 +81,9 @@ class DayLayout(context: Context, settings: CalendarSettings, var month: Date) :
             val cal: Calendar = CalendarKt.getInstance(settings).apply {
                 time = month
                 set(Calendar.DAY_OF_MONTH, 1)
-                add(Calendar.DAY_OF_YEAR, (-this[Calendar.DAY_OF_WEEK] + dayOfWeekOffset + 1).let { if (it > 0) (it - WeekDay.values().size) else it })
+                add(Calendar.DAY_OF_YEAR, (-this[Calendar.DAY_OF_WEEK] + dayOfWeekOffset + 1).let { offset ->
+                    if (offset > 0) (offset - WeekDay.values().size) else offset
+                })
             }
             firstDate = cal
 
